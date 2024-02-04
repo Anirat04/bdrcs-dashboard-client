866cc0ea7b77976877c021dea22010d22ab375b1
Tanbir Haque <tanbirhaque53@gmail.com>
2024-02-05 01:06:20 +0600

    Customized the search field and navbar of the dashboard

--- src/DashboardLayout/DashboardLayout.jsx
+++ src/DashboardLayout/DashboardLayout.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import SideBar from './Shared/SideBar/SideBar';
 import { Outlet } from 'react-router-dom';
+import NavBar from './Shared/NavBar/NavBar';
 
 const DashboardLayout = () => {
     return (
@@ -12,6 +13,7 @@ const DashboardLayout = () => {
                 </div>
                 {/* Dashboard main content starts here */}
                 <div className='bg-[#F8F8F8] w-full'>
+                    <NavBar></NavBar>
                     <Outlet></Outlet>
                 </div>
             </div>
